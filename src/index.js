520d279fba66793e9f08862c1e45f9c938b65254
vrx_lasiqueira@uolinc.com <leandroaps@gmail.com>
2019-07-23 14:51:57 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Card } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row mt-5">
+          <div className="col-sm">
+            <h1 className="text-center mb-3">Hello, World.</h1>
+            <Card style={{ maxWidth: '50%' }} className="mx-auto">
+              <Card.Header>React Parcel Bootstrap</Card.Header>
+              <Card.Body>
+                <Card.Subtitle className="mb-4 text-muted">
+                  A Parcel.js Example Built with React
+                </Card.Subtitle>
+                <Card.Text>
+                  Setup of a React Parcel and built an app in 5 minutes and you
+                  can, too! Click the button below to learn more!
+                </Card.Text>
+                <Card.Link
+                  className="text-success"
+                  href="https://github.com/leandroaps/react-parcel"
+                >
+                  Learn More
+                </Card.Link>
+              </Card.Body>
+            </Card>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mountApp = document.getElementById('app');
+ReactDOM.render(<App />, mountApp);
